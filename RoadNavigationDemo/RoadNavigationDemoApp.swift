91d3ae7d3b826f94af33eb62833ddc35a120eab7
ondravit <ondra.vit04@seznam.cz>
2025-10-07 19:26:41 +0200

    Initial Commit

---
+++ RoadNavigationDemo/RoadNavigationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoadNavigationDemoApp.swift
+//  RoadNavigationDemo
+//
+//  Created by Ondřej Vít on 07.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct RoadNavigationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
